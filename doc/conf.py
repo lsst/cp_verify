650dfc72f82777e22eb3f4e0093bcf9c225c7158
Kian-Tat Lim <ktl@slac.stanford.edu>
2021-07-16 13:33:17 -0500

    Clean up doc dir.

--- doc/conf.py
+++ doc/conf.py
@@ -1,13 +1,15 @@
 """Sphinx configuration file for an LSST stack package.
 
 This configuration only affects single-package Sphinx documentation builds.
+For more information, see:
+https://developer.lsst.io/stack/building-single-package-docs.html
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
-import lsst.cp.verify
+
+from documenteer.conf.pipelinespkg import *
 
 
-_g = globals()
-_g.update(build_package_configs(
-    project_name='lsst.cp.verify',
-    version=lsst.cp.verify.version.__version__))
+project = "cp_verify"
+html_theme_options["logotext"] = project
+html_title = project
+html_short_title = project
