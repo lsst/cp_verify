39960865a6ab7881da6837b9af2d44f88f1803f2
Christopher Waters <czw@princeton.edu>
2021-11-19 17:00:36 -0600

    Fix scipy build error.

--- python/lsst/cp/verify/verifyDefects.py
+++ python/lsst/cp/verify/verifyDefects.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import numpy as np
-import scipy
+import scipy.stats
 
 from .verifyStats import CpVerifyStatsConfig, CpVerifyStatsTask, CpVerifyStatsConnections
 
