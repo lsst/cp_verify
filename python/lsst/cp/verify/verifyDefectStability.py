4f06bd1a9defd17cf3a1defca0e486dd50fd599f
Sean MacBride <sean.p.macbride@gmail.com>
2026-04-07 22:16:57 +0000

    Adding updates to defect stability task, using new lookup function

--- python/lsst/cp/verify/verifyDefectStability.py
+++ python/lsst/cp/verify/verifyDefectStability.py
@@ -23,6 +23,7 @@ import numpy as np
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
 import lsst.pipe.base.connectionTypes as cT
+import lsst.afw.image as afwImage
 import pandas as pd
 from lsst.analysis.tools.actions.plot import FocalPlaneGeometryPlot as FPGPlot
 from .verifyDefects import (
@@ -35,30 +36,52 @@ from .verifyStats import (
 __all__ = ["CpMeasureDefectsStabilityTaskConfig", "CpMeasureDefectsStabilityTask"]
 
 
+def lookupStaticCalibrations(datasetType, registry, quantumDataId, collections):
+    # This will retrieve all the filter transmissions available
+    # associated with the {instrument, detector} dimensions.
+    timespan = Timespan(begin=None, end=None)
+    result = []
+    # First iterate over all of the data IDs for this dataset type that are
+    # consistent with the quantum data ID.
+    for dataId in registry.queryDataIds(datasetType.dimensions, dataId=quantumDataId):
+        # Find the dataset with this data ID using the unbounded timespan.
+        if ref := registry.findDataset(
+            datasetType, dataId, collections=collections, timespan=timespan
+        ):
+            result.append(ref)
+    return result
+
+
 class CpMeasureDefectsStabilityConnections(
     pipeBase.PipelineTaskConnections,
     dimensions=("instrument", "detector"),  # TODO: Update the inheritance
 ):
-    referenceDefectsExp = cT.PrerequisiteInput(
-        name="referenceExp",
-        doc="Reference exposure to extract defects from.",
-        storageClass="Exposure",
+    referenceDefects = cT.PrerequisiteInput(
+        name="defects",
+        doc="Reference defects.",
+        storageClass="Defects",
         dimensions=(
             "instrument",
             "detector",
         ),
+        lookupFunction=lookupStaticCalibrations,
+        deferLoad=True,
         multiple=True,
     )
-    productionDefectsExp = cT.PrerequisiteInput(
-        name="productionDefects",
-        doc="Production exposure to extract defects from.",
-        storageClass="Exposure",
+
+    productionDefects = cT.PrerequisiteInput(
+        name="defects",
+        doc="Production defects.",
+        storageClass="Defects",
         dimensions=(
             "instrument",
             "detector",
         ),
+        lookupFunction=lookupStaticCalibrations,
+        deferLoad=True,
         multiple=True,
     )
+
     camera = cT.PrerequisiteInput(
         name="camera",
         doc="Camera associated with these defects.",
@@ -86,13 +109,13 @@ class CpMeasureDefectsStabilityTaskConfig(
 
     refLabel = pexConfig.Field(
         dtype=str,
-        doc="Label to apply to the reference exposure.",
+        doc="Label to apply to the reference defect mask.",
         optional=False,
     )
 
     prodLabel = pexConfig.Field(
         dtype=str,
-        doc="Label to apply to the production exposure.",
+        doc="Label to apply to the production defect mask.",
         optional=False,
     )
 
@@ -150,9 +173,7 @@ class CpMeasureDefectsStabilityTaskConfig(
         super().validate()
 
 
-class CpMeasureDefectsStabilityTask(
-    pipeBase.PipelineTask
-):  # I dropped MergeDefectsTask inheritance here
+class CpMeasureDefectsStabilityTask(pipeBase.PipelineTask):
     """Task to measure variation  defects in combined images."""
 
     ConfigClass = CpMeasureDefectsStabilityTaskConfig
@@ -212,99 +233,98 @@ class CpMeasureDefectsStabilityTask(
     def processDetectorsAndAmps(self, referenceDefects, productionDefects, defectData):
         for detectorName, detectorObject in self.config.detName_Obj.items():
             # Select identical detectors
-            exp1 = referenceDefects[
+            defect1 = referenceDefects[
                 [
-                    x.getDetector().getId() == detectorObject.getId()
+                    x.getMetadata()["DETECTOR"] == detectorObject.getId()
                     for x in referenceDefects
                 ]
             ]
-            exp2 = productionDefects[
+            defect2 = productionDefects[
                 [
-                    x.getDetector().getId() == detectorObject.getId()
+                    x.getMetadata()["DETECTOR"] == detectorObject.getId()
                     for x in productionDefects
                 ]
             ]
-            try:
 
-                # Make two mask objects that possess the defect masks
-                mask1 = exp1.maskedImage.mask
-                mask2 = exp2.maskedImage.mask
+            # Make two mask objects that possess the defect masks
+            exp1 = afwImage.ExposureF(detectorObject.getBBox())
+            defect1.maskPixels(exp1.maskedImage)
+            mask1 = exp1.maskedImage.mask
 
-                """
-                # Quick check on the nature of the two defect masks
-                # If they are identical, there's probably an
-                # elegant way to speed this up
-                if all((mask1.array - mask2.array).flatten() == 0):
-                    print("Mask planes are identical.")
-                else:
-                    print("Mask planes are not identical.")
-                # NOTE: Commenting the above out for now, there is definitely
-                # a faster way to address identical mask planes...
-                """
+            exp2 = afwImage.ExposureF(detectorObject.getBBox())
+            defect2.maskPixels(exp2.maskedImage)
+            mask2 = exp2.maskedImage.mask
 
-                # Assert that the masks have the same plane definitions
-                assert (
-                    mask1.getMaskPlaneDict() == mask2.getMaskPlaneDict()
-                ), "Mask plane dictionaries are incompatible"
+            del exp1, exp2
 
-                # For each amp
-                for amp in detectorObject.getAmplifiers():
+            """
+            # Quick check on the nature of the two defect masks
+            # If they are identical, there's probably an
+            # elegant way to speed this up
+            if all((mask1.array - mask2.array).flatten() == 0):
+                print("Mask planes are identical.")
+            else:
+                print("Mask planes are not identical.")
+            # NOTE: Commenting the above out for now, there is definitely
+            # a faster way to address identical mask planes...
+            """
 
-                    if self.config.includeAny:
-                        # Instantiate the anymask object
-                        anyMasks = []
-                        for acq in [self.config.refLabel, self.config.prodLabel]:
-                            anyMasks.append(
-                                np.zeros(np.shape(mask1[amp.getBBox()].array))
-                            )
+            # Assert that the masks have the same plane definitions
+            assert (
+                mask1.getMaskPlaneDict() == mask2.getMaskPlaneDict()
+            ), "Mask plane dictionaries are incompatible"
 
-                    # Get the amplifier name and compute the
-                    # variability inside that amplifier region
-                    ampName = amp.getName()
-                    variability = self.evaluate_mask_variability(
-                        [mask1[amp.getBBox()].array, mask2[amp.getBBox()].array],
-                        [mask1.getMaskPlaneDict(), mask2.getMaskPlaneDict()],
-                    )
+            # For each amp
+            for amp in detectorObject.getAmplifiers():
 
-                    # For each mask plane
-                    for plane, planeVals in variability.items():
-                        if plane not in self.config.skipMasks:
-                            # Compute NPix for each run and add it to the df
-                            for i, acq in enumerate([self.config.refLabel, self.config.prodLabel]):
-                                defectData.loc[
-                                    f"{detectorName}_{ampName}", f"{acq}_{plane}"
-                                ] = np.count_nonzero(
-                                    planeVals[i]
-                                )
-                                # N pix that are this mask plane type, in
-                                # this acq run, indexed by amplifier
-                                anyMasks[i] += planeVals[i]
+                if self.config.includeAny:
+                    # Instantiate the anymask object
+                    anyMasks = []
+                    for acq in [self.config.refLabel, self.config.prodLabel]:
+                        anyMasks.append(np.zeros(np.shape(mask1[amp.getBBox()].array)))
 
-                            # Compute the difference and add it to the df
+                # Get the amplifier name and compute the
+                # variability inside that amplifier region
+                ampName = amp.getName()
+                variability = self.evaluate_mask_variability(
+                    [mask1[amp.getBBox()].array, mask2[amp.getBBox()].array],
+                    [mask1.getMaskPlaneDict(), mask2.getMaskPlaneDict()],
+                )
+
+                # For each mask plane
+                for plane, planeVals in variability.items():
+                    if plane not in self.config.skipMasks:
+                        # Compute NPix for each run and add it to the df
+                        for i, acq in enumerate(
+                            [self.config.refLabel, self.config.prodLabel]
+                        ):
                             defectData.loc[
-                                f"{detectorName}_{ampName}", f"{self.config.acqString}{plane}"
-                            ] = np.sum(
-                                planeVals[i - 1] - planeVals[i], dtype=int
-                            )
-                            # Difference in N pix that are this
-                            # mask plane type, indexed by amplifier
+                                f"{detectorName}_{ampName}", f"{acq}_{plane}"
+                            ] = np.count_nonzero(planeVals[i])
+                            # N pix that are this mask plane type, in
+                            # this acq run, indexed by amplifier
+                            anyMasks[i] += planeVals[i]
 
-                    if self.config.includeAny:
-                        # Perform the same calculation,
-                        # here for the anyMask case
-                        for i, acq in enumerate([self.config.refLabel, self.config.prodLabel]):
-                            defectData.loc[
-                                f"{detectorName}_{ampName}", f"{acq}_Any"
-                            ] = np.count_nonzero(anyMasks[i])
+                        # Compute the difference and add it to the df
                         defectData.loc[
-                            f"{detectorName}_{ampName}", f"{self.config.acqString}Any"
-                        ] = np.sum(anyMasks[i - 1] - anyMasks[i], dtype=int)
+                            f"{detectorName}_{ampName}",
+                            f"{self.config.acqString}{plane}",
+                        ] = np.sum(planeVals[i - 1] - planeVals[i], dtype=int)
+                        # Difference in N pix that are this
+                        # mask plane type, indexed by amplifier
 
-                # print(f"Completed processing {detectorName}")
-
-            except Exception:  # as e:
-                # print(f"Sensor {detectorName} failed during processing: {e}")
-                continue
+                if self.config.includeAny:
+                    # Perform the same calculation,
+                    # here for the anyMask case
+                    for i, acq in enumerate(
+                        [self.config.refLabel, self.config.prodLabel]
+                    ):
+                        defectData.loc[f"{detectorName}_{ampName}", f"{acq}_Any"] = (
+                            np.count_nonzero(anyMasks[i])
+                        )
+                    defectData.loc[
+                        f"{detectorName}_{ampName}", f"{self.config.acqString}Any"
+                    ] = np.sum(anyMasks[i - 1] - anyMasks[i], dtype=int)
 
         return defectData
 
